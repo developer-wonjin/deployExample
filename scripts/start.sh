19ea0d432457761bc022fbbd2ea01d33effbd677
wonjin <wonjin1776@naver.com>
2021-08-18 07:42:40 +0900

    수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -30,3 +30,6 @@ nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
+
+#   $REPOSITORY/$JAR_NAME 2>&1 &
